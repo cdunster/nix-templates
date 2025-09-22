33e199a1d66cb206dd39cbedd5ba9fa944f187f1
Callum Dunster <cdunster@users.noreply.github.com>
2025-09-22 11:18:21 +0200

    Use flake-parts instead of flake-utils in base rust template

--- rust/flake.nix
+++ rust/flake.nix
@@ -1,32 +1,35 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
     };
   };
 
-  outputs = { self, ... }@inputs:
-    inputs.flake-utils.lib.eachDefaultSystem (system:
+  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+    systems = inputs.nixpkgs.lib.systems.flakeExposed;
+
+    perSystem = { pkgs, system, ... }:
       let
-        pkgs = inputs.nixpkgs.legacyPackages.${system}.appendOverlays [
-          inputs.rust-overlay.overlays.default
-        ];
         rustFromFile = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
       in
       {
-        devShell = with pkgs; mkShell {
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [ inputs.rust-overlay.overlays.default ];
+        };
+
+        formatter = pkgs.nixpkgs-fmt;
+
+        devShells.default = with pkgs; mkShell {
           packages = [
             rustFromFile
-            cargo-audit
-            cargo-edit
-            cargo-watch
           ];
 
-          RUST_SRC_PATH = rustPlatform.rustLibSrc;
+          RUST_SRC_PATH = "${rustFromFile}/lib/rustlib/src/rust";
         };
-      });
+      };
+  };
 }
