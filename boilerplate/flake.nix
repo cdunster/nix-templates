0de15c1867ed2b96e0a2003a5bac637583ba02b3
Callum Dunster <cdunster@users.noreply.github.com>
2025-09-22 11:18:03 +0200

    Use flake-parts instead of flake-utils in boilerplate template

--- boilerplate/flake.nix
+++ boilerplate/flake.nix
@@ -1,18 +1,19 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        devShell = with pkgs; mkShell {
-          buildInputs = [
-          ];
-        };
-      });
+  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+    systems = inputs.nixpkgs.lib.systems.flakeExposed;
+
+    perSystem = { pkgs, ... }: {
+      formatter = pkgs.nixpkgs-fmt;
+
+      devShells.default = with pkgs; mkShell {
+        packages = [
+        ];
+      };
+    };
+  };
 }
