3cc36fc5d1befd1dc07920105092d55f6989ca7b
Callum Dunster <cdunster@users.noreply.github.com>
2025-09-22 11:20:34 +0200

    Add git-hooks to boilerplate template

--- boilerplate/flake.nix
+++ boilerplate/flake.nix
@@ -2,17 +2,50 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } {
     systems = inputs.nixpkgs.lib.systems.flakeExposed;
 
-    perSystem = { pkgs, ... }: {
+    imports = [
+      inputs.git-hooks.flakeModule
+    ];
+
+    perSystem = { config, pkgs, ... }: {
       formatter = pkgs.nixpkgs-fmt;
+
+      pre-commit.settings.hooks = {
+        # Nix
+        nixpkgs-fmt.enable = true;
+        deadnix.enable = true;
+        statix.enable = true;
+
+        # Git
+        check-merge-conflicts.enable = true;
+        no-commit-to-branch.enable = true;
+        commitizen.enable = true;
+
+        # Spell checking
+        typos.enable = true;
+
+        # Whitespace
+        mixed-line-endings.enable = true;
+        trim-trailing-whitespace.enable = true;
+
+        # Private keys
+        detect-private-keys.enable = true;
+      };
 
       devShells.default = with pkgs; mkShell {
         packages = [
+          config.pre-commit.settings.enabledPackages
         ];
+
+        shellHook = "${config.pre-commit.installationScript}";
       };
     };
   };
