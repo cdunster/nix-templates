bf960d9b345320d7be07e5d110922d706f69b31d
Callum Dunster <cdunster@users.noreply.github.com>
2025-09-22 11:22:02 +0200

    Add git-hooks to base rust template

--- rust/flake.nix
+++ rust/flake.nix
@@ -6,12 +6,20 @@
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } {
     systems = inputs.nixpkgs.lib.systems.flakeExposed;
 
-    perSystem = { pkgs, system, ... }:
+    imports = [
+      inputs.git-hooks.flakeModule
+    ];
+
+    perSystem = { config, pkgs, system, ... }:
       let
         rustFromFile = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
       in
@@ -23,12 +31,43 @@
 
         formatter = pkgs.nixpkgs-fmt;
 
+        pre-commit.settings.hooks = {
+          # Nix
+          nixpkgs-fmt.enable = true;
+          deadnix.enable = true;
+          statix.enable = true;
+
+          # Rust
+          rustfmt.enable = true;
+
+          # Git
+          check-merge-conflicts.enable = true;
+          no-commit-to-branch.enable = true;
+          commitizen.enable = true;
+
+          # TOML
+          taplo.enable = true;
+
+          # Spell checking
+          typos.enable = true;
+
+          # Whitespace
+          mixed-line-endings.enable = true;
+          trim-trailing-whitespace.enable = true;
+
+          # Private keys
+          detect-private-keys.enable = true;
+        };
+
         devShells.default = with pkgs; mkShell {
           packages = [
+            config.pre-commit.settings.enabledPackages
             rustFromFile
           ];
 
           RUST_SRC_PATH = "${rustFromFile}/lib/rustlib/src/rust";
+
+          shellHook = "${config.pre-commit.installationScript}";
         };
       };
   };
